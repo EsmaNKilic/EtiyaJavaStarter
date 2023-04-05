2036c25aebfaeada56cc93565551bc19188ecabb
Esma <50068550+EsmaNKilic@users.noreply.github.com>
2023-04-05 16:15:11 +0300

    Delete EtiyaJavaStarter directory

--- EtiyaJavaStarter/helloWorld/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Merhaba Esma!");
-        System.out.println("Tekrar Merhaba Java!");
-    }
-}
--- EtiyaJavaStarter/variables/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //reusability
-        int kursSayisi = 15;
-        String mesaj = "Kurs sayım : ";
-        System.out.println(mesaj + kursSayisi);
-    }
-}
